1849801880ba327345af66167ef84831b20f00f6
Pallav Tyagi <pallavtyagiofficial@gmail.com>
2026-08-25 18:51:33 +0530

    Updated gradient calculation and the add/multiplication capability of the class.

--- autograd.py
+++ autograd.py
@@ -7,31 +7,49 @@ class Value:
     def __init__(self, data, parent_nodes = ()):
         self.data = data
         self.gradient = 0.0
-        self._back_propagation = lambda: None   # A variable that store an empty function. This prevents _backpropagation()
-                                                # call for the leaf nodes.
+        # A variable that store an empty function. This prevents _backpropagation() call for the leaf nodes.
+        self._back_propagation = lambda: None                                                
         self._parent_node = set(parent_nodes)
 
+    # IMPLEMENTING ADDITION.
     def __add__(self, other):
+        # If other is anything else than an object of class Value (like '2' in expression 'a+2'),
+        # we make it one.
+        other = other if isinstance(other, Value) else Value(other)
         output = Value(self.data + other.data, (self, other))
 
         def _back_propagation():
-            self.gradient = output.gradient
-            other.gradient = output.gradient
+            # '+=' for Gradient Accumulation.
+            self.gradient += output.gradient    
+            other.gradient += output.gradient
 
         output._back_propagation = _back_propagation
 
         return output
 
+    # A routing function for a condition like '2+a'.
+    def __radd__(self, other):
+        return self + other
+
+    # IMPLEMENTING MULTIPLICATION.
     def __mul__(self, other):
+        # If other is anything else than an object of class Value (like '2' in expression 'a*2'),
+        # we make it one.
+        other = other if isinstance(other, Value) else Value(other)
         output = Value(self.data * other.data, (self, other))
 
         def _back_propagation():
-            self.gradient = other.data * output.gradient
-            other.gradient = self.data * output.gradient
+            # '+=' for Gradient Accumulation.
+            self.gradient += other.data * output.gradient
+            other.gradient += self.data * output.gradient
 
         output._back_propagation = _back_propagation
 
         return output
+
+    # A routing function for a condition like '2*a'.
+    def __rmul__(self, other):
+        return self * other
 
     def tanh(self):
         x = self.data
@@ -39,7 +57,8 @@ class Value:
         output = Value(tanh_result, (self,))
 
         def _back_propagation():
-            self.gradient = (1 - tanh_result**2) * output.gradient
+            # I am skeptical if gradient accumulation is needed here.
+            self.gradient += (1 - tanh_result**2) * output.gradient     
 
         output._back_propagation = _back_propagation
         
@@ -66,7 +85,8 @@ class Value:
 
         # Defining back_propagation()
 
-        self.gradient = 1.0     # Setting the gradient of Output Node as 1.0
+        # Setting the gradient of Output Node as 1.0
+        self.gradient = 1.0    
 
         for node in reversed(sorted_nodes):
             node._back_propagation()
